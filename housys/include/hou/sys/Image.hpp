21afaa086ee5cba94c2aaa0d81c2853407b54a64
Davide Corradi <davide.corradi.dev@gmail.com>
2018-04-10 22:32:01 +0200

    Documentation for Image.

--- housys/include/hou/sys/Image.hpp
+++ housys/include/hou/sys/Image.hpp
@@ -34,40 +34,161 @@ template <size_t dim, PixelFormat fmt>
 class HOU_SYS_API Image
 {
 public:
-  using Size = Vec<uint, dim>;
-  using Coordinates = Size;
-  using Pixel = PixelT<fmt>;
-  using PixelCollection = std::vector<Pixel>;
-
   template <size_t otherDim, PixelFormat otherFmt>
   friend class Image;
 
+  /** Type representing the size of the Image. */
+  using Size = Vec<uint, dim>;
+
+  /** Type representing pixel coordinates. */
+  using Coordinates = Size;
+
+  /** Type of the image pixels. */
+  using Pixel = PixelT<fmt>;
+
+  /** Type used to represent the collection of pixels of the image. */
+  using PixelCollection = std::vector<Pixel>;
+
 public:
   static constexpr size_t getDimensionCount();
+
+  /** Retrieves the format of the pixels of the image.
+   *
+   *  \return the format of the pixels of the image.
+   */
   static constexpr PixelFormat getPixelFormat();
+
+  /** Retrieves the amount of bytes used by a pixel of the image.
+   *
+   * \return the amomunt of bytes used by a pixel of the image.
+   */
   static constexpr uint getPixelByteCount();
 
 public:
+  /** Default constructor.
+   *
+   *  Creates an image with no pixels.
+   *  All of its size components are equal to 0.
+   */
   Image();
+
+  /** Size constructor.
+   *
+   *  Creates an Image with the given size.
+   *  All pixel channels are initialized to 0.
+   *
+   *  \param size the size of the image.
+   */
   Image(const Size& size);
+
+  /** Pixel constructor.
+   *
+   *  Creates an image with the given size, with all pixels set to the given
+   *  value.
+   *
+   *  \param size the size of the image.
+   *  \param pixel the value of the pixels of the image.
+   */
   Image(const Size& size, const Pixel& pixel);
+
+  /** Pixels constructor.
+   *
+   *  Creates an image with the given size and pixels.
+   *  Throws if the size of pixels does not coincide the the product of all
+   *  elements of size.
+   *  The collection contains a linear list of pixels.
+   *  Pixels along the first axis must be adjacent in the collection.
+   *
+   *  \param size the size of the image.
+   *  \param pixels the collection of pixels of the image.
+   */
   Image(const Size& size, const Span<const Pixel>& pixels);
+
+  /** Conversion constructor.
+   *
+   *  Creates an image from an image with a different number of dimensions
+   * and/or format. If the pixel format is different, each pixel is converted
+   * according to the conversion rule of the Pixel class. If the number of
+   * dimensions of other is lower, the generated image has size 1 on all
+   * additional dimensions. It is not possible to create an Image from an Image
+   * with more dimensions.
+   *
+   *  \tparam otherDim the number of dimensions of the other image.
+   *  \tparam otherFmt the pixel format of the other image.
+   *  \tparam Enable enabling parameter.
+   *
+   *  \param other the other image.
+   */
   template <size_t otherDim, PixelFormat otherFmt,
     typename Enable = std::enable_if_t<(otherFmt != fmt || otherDim != dim)
       && (otherDim <= dim)>>
   HOU_SYS_API Image(const Image<otherDim, otherFmt>& other);
 
+  /** Retrieves the size of the image.
+   *
+   *  \return the size of the image.
+   */
   const Size& getSize() const;
 
+  /** Retrieves the pixels of the image.
+   *
+   *  \return tehe pixels of the image.
+   */
   const PixelCollection& getPixels() const;
+
+  /** Sets the pixels of the image.
+   *
+   *  Throws if the size of pixels is not equal to the product of all elements
+   *  of the size of the image.
+   *
+   *  \param pixels the pixels.
+   */
   void setPixels(const Span<const Pixel>& pixels);
 
+  /** Retrieves a single pixel.
+   *
+   *  Throws if the coordinates exceed the size of the image.
+   *
+   *  \param coordinates the coordinates of the pixel.
+   *  \return tue pixel.
+   */
   const Pixel& getPixel(const Coordinates& coordinates) const;
+
+  /** Sets the value of a single pixel.
+   *
+   *  Throws if the coordinates exceed the size of the image.
+   *
+   *  \param coordinates the coordinates of the pixel.
+   *  \param value the value of the pixel.
+   */
   void setPixel(const Coordinates& coordinates, const Pixel& value);
 
+  /** Retrieves a sub-image.
+   *
+   *  Throws if the sum of offset and size is greater or equal than the size of
+   *  the image.
+   *
+   *  \param offset the offset of the sub-image.
+   *  \param size the size of the sub-image.
+   *  \return the sub-image starting at offset (included) and with the specified
+   *  size.
+   */
   Image getSubImage(const Coordinates& offset, const Size& size);
+
+  /** Sets a region of the image with the given sub-image.
+   *
+   *  Throws if the sum of offset and the size of the input image is greater or
+   * equal than the size of the image.
+   *
+   *  \param offset the offset of the sub-image.
+   *  \param image the sub-image to be copied.
+   */
   void setSubImage(const Coordinates& offset, const Image& image);
 
+  /** Clears the whole image with the given pixel value.
+   *
+   *  \param pixel the pixel value.
+   */
   void clear(const Pixel& pixel);
 
 private:
@@ -79,17 +200,41 @@ private:
   PixelCollection mPixels;
 };
 
+/** Checks if two Image objects are equal.
+ *
+ *  \tparam dim the number of dimensions of the Image.
+ *  \tparam fmt the pixel format.
+ *  \param lhs the left operand.
+ *  \param rhs the right operand.
+ *  \return the result of the check.
+ */
 template <size_t dim, PixelFormat fmt>
 HOU_SYS_API bool operator==(
   const Image<dim, fmt>& lhs, const Image<dim, fmt>& rhs);
+
+/** Checks if two Image objects are not equal.
+ *
+ *  \tparam dim the number of dimensions of the Image.
+ *  \tparam fmt the pixel format.
+ *  \param lhs the left operand.
+ *  \param rhs the right operand.
+ *  \return the result of the check.
+ */
 template <size_t dim, PixelFormat fmt>
 HOU_SYS_API bool operator!=(
   const Image<dim, fmt>& lhs, const Image<dim, fmt>& rhs);
+
+/** Writes the object into a stream.
+ *
+ *  \tparam dim the number of dimensions of the Image.
+ *  \tparam fmt the pixel format.
+ *  \param os the stream.
+ *  \param im the Image..
+ *  \return a reference to os.
+ */
 template <size_t dim, PixelFormat fmt>
 HOU_SYS_API std::ostream& operator<<(
   std::ostream& os, const Image<dim, fmt>& im);
-
-
 
 /** Checks if a file is a BMP file.
  *
