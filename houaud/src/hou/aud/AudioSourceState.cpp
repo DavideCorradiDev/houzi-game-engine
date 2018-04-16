2fd8506f05f743e268ce158bc86135de1a6e85dd
Davide Corradi <davide.corradi.dev@gmail.com>
2018-04-16 23:45:12 +0200

    Output stream operator for AudioSourceState.

---
+++ houaud/src/hou/aud/AudioSourceState.cpp
@@ -0,0 +1,31 @@
+// Houzi Game Engine
+// Copyright (c) 2018 Davide Corradi
+// Licensed under the MIT license.
+
+#include "hou/aud/AudioSourceState.hpp"
+
+#include "hou/cor/CorError.hpp"
+#include "hou/cor/Error.hpp"
+
+#define AUDIO_SOURCE_STATE_CASE(state, os) \
+  case AudioSourceState::state:            \
+    return (os) << #state
+
+
+
+namespace hou
+{
+
+std::ostream& operator<<(std::ostream& os, AudioSourceState state)
+{
+  switch(state)
+  {
+    AUDIO_SOURCE_STATE_CASE(Stopped, os);
+    AUDIO_SOURCE_STATE_CASE(Playing, os);
+    AUDIO_SOURCE_STATE_CASE(Paused, os);
+  default:
+    return os;
+  }
+}
+
+}  // namespace hou
