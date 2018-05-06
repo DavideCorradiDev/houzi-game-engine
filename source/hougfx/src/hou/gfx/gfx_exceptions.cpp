20a3211d8d071b0a9279fc0890818e8a626e7c60
Davide Corradi <davide.corradi.dev@gmail.com>
2018-05-06 16:17:42 +0200

    gfx_exceptions tests.

--- source/hougfx/src/hou/gfx/gfx_exceptions.cpp
+++ source/hougfx/src/hou/gfx/gfx_exceptions.cpp
@@ -10,12 +10,12 @@ namespace hou
 {
 
 font_creation_error::font_creation_error(const std::string& path, uint line)
-  : exception(path, line, u8"Failed to create the font.")
+  : exception(path, line, u8"Failed to create the font face object.")
 {}
 
 font_destruction_error::font_destruction_error(
   const std::string& path, uint line)
-  : exception(path, line, u8"Failed to destroy the font.")
+  : exception(path, line, u8"Failed to destroy the font face object.")
 {}
 
 }  // namespace hou
---
+++ source/hougfx/test/hou/gfx/test_gfx_exceptions.cpp
@@ -0,0 +1,35 @@
+// Houzi Game Engine
+// Copyright (c) 2018 Davide Corradi
+// Licensed under the MIT license.
+
+#include "hou/test.hpp"
+
+#include "hou/gfx/gfx_exceptions.hpp"
+
+using namespace hou;
+using namespace testing;
+
+
+
+namespace
+{
+
+class test_gfx_exceptions : public Test {};
+
+}
+
+
+
+TEST_F(test_gfx_exceptions, font_creation_error)
+{
+  font_creation_error ex("foo.cpp", 88u);
+  EXPECT_STREQ("foo.cpp:88 - Failed to create the font face object.", ex.what());
+}
+
+
+
+TEST_F(test_gfx_exceptions, font_destruction_error)
+{
+  font_destruction_error ex("foo.cpp", 88u);
+  EXPECT_STREQ("foo.cpp:88 - Failed to destroy the font face object.", ex.what());
+}
