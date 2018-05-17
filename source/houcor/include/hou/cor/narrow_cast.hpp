b8a85f958cc219af8ebb4ad7cef086f9bb85c8ec
Davide Corradi <davide.corradi.dev@gmail.com>
2018-05-17 21:40:13 +0200

    is_same_signedness tests.

--- source/houcor/include/hou/cor/narrow_cast.hpp
+++ source/houcor/include/hou/cor/narrow_cast.hpp
@@ -6,6 +6,7 @@
 #define HOU_COR_NARROW_CAST_HPP
 
 #include "hou/cor/cor_exceptions.hpp"
+#include "hou/cor/is_same_signedness.hpp"
 
 #include "hou/cor/cor_config.hpp"
 
@@ -13,23 +14,6 @@
 
 namespace hou
 {
-
-/** Determines if two types have the same signedness.
- *
- * If T and U have the same signedness, provides the member constant value equal
- * true. Otherwise the value is false.
- *
- * \tparam T the first type.
- *
- * \tparam U the second type.
- */
-template <typename T, typename U>
-struct is_same_signedness
-  : public std::integral_constant<bool,
-      std::is_signed<T>::value && std::is_signed<U>::value>
-{};
-
-
 
 /** Cast checking for narrowing errors.
  *
