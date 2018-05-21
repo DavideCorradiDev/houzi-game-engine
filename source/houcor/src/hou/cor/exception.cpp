d67b98432e4bd8cc422608e2b6862dbf16404da5
Davide Corradi <davide.corradi.dev@gmail.com>
2018-05-21 17:12:03 +0200

    Call abort instead of terminate in case of errors.
    On MSVC, if exceptions are disabled, terminate will not call abort,
    and set_terminate will have no effect. get_terminate will always
    return nullptr. This makes it impossible to test error cases with
    exceptions disabled.

--- source/houcor/src/hou/cor/exception.cpp
+++ source/houcor/src/hou/cor/exception.cpp
@@ -20,8 +20,8 @@ std::string format_error_message(
   const std::string& path, uint line, const std::string& message)
 {
   std::stringstream ss;
-  ss << path.substr(path.find_last_of("/\\") + 1) << ':' << line
-     << " - " << message;
+  ss << path.substr(path.find_last_of("/\\") + 1) << ':' << line << " - "
+     << message;
   return ss.str();
 }
 
@@ -32,7 +32,7 @@ std::string assertion_message(const std::string& statement)
   return format_string(u8"Assertion failed (%s).", statement.c_str());
 }
 
-}
+}  // namespace prv
 
 
 
@@ -52,10 +52,10 @@ const char* exception::what() const noexcept
 
 
 
-void terminate(const std::string& message) noexcept
+void abort(const std::string& message) noexcept
 {
   std::cerr << message << std::endl;
-  std::terminate();
+  std::abort();
 }
 
 }  // namespace hou
