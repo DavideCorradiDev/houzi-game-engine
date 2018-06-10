ee505ef5f42c24e6b7e000c6bcd315e020aed2c2
Davide Corradi <davide.corradi.dev@gmail.com>
2018-06-10 14:19:40 +0200

    Started implementing a simple window demo.

--- source/demo/src/window_demo.cpp
+++ source/demo/src/window_demo.cpp
@@ -1,40 +1,34 @@
+// Houzi Game Engine
+// Copyright (c) 2018 Davide Corradi
+// Licensed under the MIT license.
+
+#include "hou/cor/cor_module.hpp"
+#include "hou/mth/mth_module.hpp"
 #include "hou/sys/sys_module.hpp"
 
-#include "hou/mth/rectangle.hpp"
-
-#include "hou/gfx/graphic_context.hpp"
-#include "hou/gfx/render_window.hpp"
-#include "hou/gfx/mesh2.hpp"
-#include "hou/gfx/mesh2_shader_program.hpp"
+#include "hou/sys/system_window.hpp"
 
 #include <iostream>
 
-using namespace hou;
 
 
-
-int main()
+int main(int, char**)
 {
-  sys_module::initialize();
-  call_terminate_on_exit<sys_module>();
+  hou::cor_module::initialize();
+  hou::cor_module::register_terminate_callbacks();
+  hou::mth_module::initialize();
+  hou::mth_module::register_terminate_callbacks();
+  hou::sys_module::initialize();
+  hou::sys_module::register_terminate_callbacks();
 
-  graphic_context gctx;
-  graphic_context::set_current(gctx);
+  hou::system_window w("WindowDemo", hou::vec2u(640u, 480u));
+  w.show();
 
-  mesh2_shader_program m2r;
-  mesh2 rect = create_rectangle_mesh2(vec2f(32.f, 16.f));
-
-  render_window rw("WindowDemo", vec2u(640u, 480u));
-  trans2f proj_t = trans2f::orthographic_projection(rectf(0, 0, 640, 480));
-
-  m2r.draw(rw, rect, color::red(), proj_t * trans2f::translation(vec2f(40.f, 88.f)));
-
-  rw.show();
-  rw.display();
-
-
-  std::cout << "Press any key";
+  std::cout << "Press return to resize the window";
+  std::cin.get();
+  w.set_size(hou::vec2u(800u, 600u));
+  std::cout << "Press return to end the program";
   std::cin.get();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
