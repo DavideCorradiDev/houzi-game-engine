7ecaa0ccc32ad36b89eaf722f0dd1687e49e9b0a
Davide Corradi <davide.corradi.dev@gmail.com>
2018-06-19 00:28:28 +0200

    More gl context tests.

--- source/hougl/test/hou/gl/test_gl_context.cpp
+++ source/hougl/test/hou/gl/test_gl_context.cpp
@@ -9,7 +9,6 @@
 #include "hou/gl/gl_context.hpp"
 #include "hou/gl/gl_context_settings.hpp"
 #include "hou/gl/gl_exceptions.hpp"
-#include "hou/gl/gl_functions.hpp"
 
 #include "hou/sys/system_window.hpp"
 
@@ -115,53 +114,53 @@ TEST_F(test_gl_context, get_uid)
 
 
 
-// TEST_F(test_gl_context, get_sharing_group_uid)
-// {
-//   system_window w("Test", vec2u::zero());
-//   gl::context c1(gl::context_settings::get_default(), w);
-//   gl::context c2(gl::context_settings::get_default(), w);
-//   gl::context c3(gl::context_settings::get_default(), w, c1);
-//   gl::context c4(gl::context_settings::get_default(), w, c3);
-//   gl::context c5(gl::context_settings::get_default(), w, c1);
-//   gl::context c6(gl::context_settings::get_default(), w);
-//   gl::context c7(gl::context_settings::get_default(), w, c2);
-//   gl::context c8(gl::context_settings::get_default(), w, c5);
-// 
-//   EXPECT_NE(c1.get_sharing_group_uid(), c2.get_sharing_group_uid());
-//   EXPECT_EQ(c1.get_sharing_group_uid(), c3.get_sharing_group_uid());
-//   EXPECT_EQ(c1.get_sharing_group_uid(), c4.get_sharing_group_uid());
-//   EXPECT_EQ(c1.get_sharing_group_uid(), c5.get_sharing_group_uid());
-//   EXPECT_NE(c1.get_sharing_group_uid(), c6.get_sharing_group_uid());
-//   EXPECT_NE(c1.get_sharing_group_uid(), c7.get_sharing_group_uid());
-//   EXPECT_EQ(c1.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// 
-//   EXPECT_NE(c2.get_sharing_group_uid(), c3.get_sharing_group_uid());
-//   EXPECT_NE(c2.get_sharing_group_uid(), c4.get_sharing_group_uid());
-//   EXPECT_NE(c2.get_sharing_group_uid(), c5.get_sharing_group_uid());
-//   EXPECT_NE(c2.get_sharing_group_uid(), c6.get_sharing_group_uid());
-//   EXPECT_EQ(c2.get_sharing_group_uid(), c7.get_sharing_group_uid());
-//   EXPECT_NE(c2.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// 
-//   EXPECT_EQ(c3.get_sharing_group_uid(), c4.get_sharing_group_uid());
-//   EXPECT_EQ(c3.get_sharing_group_uid(), c5.get_sharing_group_uid());
-//   EXPECT_NE(c3.get_sharing_group_uid(), c6.get_sharing_group_uid());
-//   EXPECT_NE(c3.get_sharing_group_uid(), c7.get_sharing_group_uid());
-//   EXPECT_EQ(c3.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// 
-//   EXPECT_EQ(c4.get_sharing_group_uid(), c5.get_sharing_group_uid());
-//   EXPECT_NE(c4.get_sharing_group_uid(), c6.get_sharing_group_uid());
-//   EXPECT_NE(c4.get_sharing_group_uid(), c7.get_sharing_group_uid());
-//   EXPECT_EQ(c4.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// 
-//   EXPECT_NE(c5.get_sharing_group_uid(), c6.get_sharing_group_uid());
-//   EXPECT_NE(c5.get_sharing_group_uid(), c7.get_sharing_group_uid());
-//   EXPECT_EQ(c5.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// 
-//   EXPECT_NE(c6.get_sharing_group_uid(), c7.get_sharing_group_uid());
-//   EXPECT_NE(c6.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// 
-//   EXPECT_NE(c7.get_sharing_group_uid(), c8.get_sharing_group_uid());
-// }
+TEST_F(test_gl_context, get_sharing_group_uid)
+{
+  system_window w("Test", vec2u::zero());
+  gl::context c1(gl::context_settings::get_default(), w);
+  gl::context c2(gl::context_settings::get_default(), w);
+  gl::context c3(gl::context_settings::get_default(), w, c1);
+  gl::context c4(gl::context_settings::get_default(), w, c3);
+  gl::context c5(gl::context_settings::get_default(), w, c1);
+  gl::context c6(gl::context_settings::get_default(), w);
+  gl::context c7(gl::context_settings::get_default(), w, c2);
+  gl::context c8(gl::context_settings::get_default(), w, c5);
+
+  EXPECT_NE(c1.get_sharing_group_uid(), c2.get_sharing_group_uid());
+  EXPECT_EQ(c1.get_sharing_group_uid(), c3.get_sharing_group_uid());
+  EXPECT_EQ(c1.get_sharing_group_uid(), c4.get_sharing_group_uid());
+  EXPECT_EQ(c1.get_sharing_group_uid(), c5.get_sharing_group_uid());
+  EXPECT_NE(c1.get_sharing_group_uid(), c6.get_sharing_group_uid());
+  EXPECT_NE(c1.get_sharing_group_uid(), c7.get_sharing_group_uid());
+  EXPECT_EQ(c1.get_sharing_group_uid(), c8.get_sharing_group_uid());
+
+  EXPECT_NE(c2.get_sharing_group_uid(), c3.get_sharing_group_uid());
+  EXPECT_NE(c2.get_sharing_group_uid(), c4.get_sharing_group_uid());
+  EXPECT_NE(c2.get_sharing_group_uid(), c5.get_sharing_group_uid());
+  EXPECT_NE(c2.get_sharing_group_uid(), c6.get_sharing_group_uid());
+  EXPECT_EQ(c2.get_sharing_group_uid(), c7.get_sharing_group_uid());
+  EXPECT_NE(c2.get_sharing_group_uid(), c8.get_sharing_group_uid());
+
+  EXPECT_EQ(c3.get_sharing_group_uid(), c4.get_sharing_group_uid());
+  EXPECT_EQ(c3.get_sharing_group_uid(), c5.get_sharing_group_uid());
+  EXPECT_NE(c3.get_sharing_group_uid(), c6.get_sharing_group_uid());
+  EXPECT_NE(c3.get_sharing_group_uid(), c7.get_sharing_group_uid());
+  EXPECT_EQ(c3.get_sharing_group_uid(), c8.get_sharing_group_uid());
+
+  EXPECT_EQ(c4.get_sharing_group_uid(), c5.get_sharing_group_uid());
+  EXPECT_NE(c4.get_sharing_group_uid(), c6.get_sharing_group_uid());
+  EXPECT_NE(c4.get_sharing_group_uid(), c7.get_sharing_group_uid());
+  EXPECT_EQ(c4.get_sharing_group_uid(), c8.get_sharing_group_uid());
+
+  EXPECT_NE(c5.get_sharing_group_uid(), c6.get_sharing_group_uid());
+  EXPECT_NE(c5.get_sharing_group_uid(), c7.get_sharing_group_uid());
+  EXPECT_EQ(c5.get_sharing_group_uid(), c8.get_sharing_group_uid());
+
+  EXPECT_NE(c6.get_sharing_group_uid(), c7.get_sharing_group_uid());
+  EXPECT_NE(c6.get_sharing_group_uid(), c8.get_sharing_group_uid());
+
+  EXPECT_NE(c7.get_sharing_group_uid(), c8.get_sharing_group_uid());
+}
 
 
 
@@ -226,113 +225,108 @@ TEST_F(test_gl_context, current_gl_context)
 
 
 
-// TEST_F(test_gl_context, single_context_multiple_windows)
-// {
-//   system_window w1(
-//     "Test", video_mode(vec2u(10u, 10u), 4u), window_style::windowed);
-//   system_window w2(
-//     "Test", video_mode(vec2u(10u, 10u), 4u), window_style::windowed);
-//   gl::context c(gl::context_settings::get_default(), w1);
-// 
-//   gl::context::set_current(c, w1);
-//   EXPECT_TRUE(c.is_current());
-// 
-//   gl::context::set_current(c, w2);
-//   EXPECT_TRUE(c.is_current());
-// }
-// 
-// 
-// 
-// TEST_F(test_gl_context, multiple_contexts_single_window)
-// {
-//   system_window w(
-//     "Test", video_mode(vec2u(10u, 10u), 4u), window_style::windowed);
-//   gl::context c1(gl::context_settings::get_default(), w);
-//   gl::context c2(gl::context_settings::get_default(), w);
-// 
-//   gl::context::set_current(c1, w);
-//   EXPECT_TRUE(c1.is_current());
-// 
-//   gl::context::set_current(c2, w);
-//   EXPECT_TRUE(c2.is_current());
-// }
-// 
-// 
-// 
-// TEST_F(test_gl_context_death_test, make_current_error)
-// {
-//   system_window w1(
-//     "Test", video_mode(vec2u(10u, 10u), 4u), window_style::windowed);
-//   gl::context c(gl::context_settings::get_default(), w1);
-// 
-//   gl::context::set_current(c, w1);
-//   ASSERT_TRUE(c.is_current());
-// 
-//   std::thread t([&c]() {
-//     system_window w2(
-//       "Test", video_mode(vec2u(10u, 10u), 4u), window_style::windowed);
-//     EXPECT_ERROR_0(gl::context::set_current(c, w2), gl::context_switch_error);
-//   });
-// 
-//   t.join();
-// }
-// 
-// 
-// 
-// TEST_F(test_gl_context_optimizations, redundant_binding)
-// {
-//   const uint calls = 1000u;
-// 
-//   system_window w1(
-//     "Test", video_mode(vec2u(4u, 4u), 4u), window_style::windowed);
-//   system_window w2(
-//     "Test", video_mode(vec2u(4u, 4u), 4u), window_style::windowed);
-//   gl::context c1(gl::context_settings::get_default(), w1);
-//   gl::context c2(gl::context_settings::get_default(), w1);
-// 
-//   // First binding appears to be more expensive.
-//   // Doing it here reduces bias in the measurements.
-//   gl::context::set_current(c1, w1);
-//   gl::context::set_current(c1, w2);
-//   gl::context::set_current(c2, w1);
-//   gl::context::set_current(c2, w2);
-// 
-//   // Measurement p_clock.
-//   hou::clock p_clock;
-// 
-//   // Bind to same context window (should not rebind).
-//   gl::context::unset_current();
-//   p_clock = hou::clock();
-//   for(uint i = 0; i < calls; ++i)
-//   {
-//     gl::context::set_current(c1, w1);
-//     gl::context::set_current(c1, w1);
-//   }
-//   std::chrono::nanoseconds time1 = p_clock.get_elapsed_time();
-// 
-//   // Bind to different window (should rebind).
-//   gl::context::unset_current();
-//   p_clock = hou::clock();
-//   for(uint i = 0; i < calls; ++i)
-//   {
-//     gl::context::set_current(c1, w1);
-//     gl::context::set_current(c1, w2);
-//   }
-//   std::chrono::nanoseconds time2 = p_clock.get_elapsed_time();
-// 
-//   // Bind to different context (should rebind).
-//   gl::context::unset_current();
-//   p_clock = hou::clock();
-//   for(uint i = 0; i < calls; ++i)
-//   {
-//     gl::context::set_current(c1, w1);
-//     gl::context::set_current(c2, w1);
-//   }
-//   std::chrono::nanoseconds time3 = p_clock.get_elapsed_time();
-// 
-//   EXPECT_LT(time1, time2);
-//   EXPECT_LT(time1, time3);
-// }
-// 
-// // Failing creation test.
-// // Failing destruction test.
+TEST_F(test_gl_context_death_test, set_current_error)
+{
+  system_window w1("Test", vec2u(10u, 10u));
+  gl::context c(gl::context_settings::get_default(), w1);
+
+  gl::context::set_current(c, w1);
+  ASSERT_TRUE(c.is_current());
+
+  std::thread t([&c]() {
+    system_window w2("Test", vec2u(10u, 10u));
+    EXPECT_ERROR_N(gl::context::set_current(c, w2), gl::context_switch_error,
+      "Unable to make GL context current");
+  });
+
+  t.join();
+}
+
+
+
+TEST_F(test_gl_context, single_context_multiple_windows)
+{
+  system_window w1("Test", vec2u(10u, 10u));
+  system_window w2("Test", vec2u(10u, 10u));
+  gl::context c(gl::context_settings::get_default(), w1);
+
+  gl::context::set_current(c, w1);
+  EXPECT_TRUE(c.is_current());
+  EXPECT_EQ(w1.get_uid(), gl::context::get_current_window_uid());
+
+  gl::context::set_current(c, w2);
+  EXPECT_TRUE(c.is_current());
+  EXPECT_EQ(w2.get_uid(), gl::context::get_current_window_uid());
+}
+
+
+
+TEST_F(test_gl_context, multiple_contexts_single_window)
+{
+  system_window w("Test", vec2u(10u, 10u));
+  gl::context c1(gl::context_settings::get_default(), w);
+  gl::context c2(gl::context_settings::get_default(), w);
+
+  gl::context::set_current(c1, w);
+  EXPECT_TRUE(c1.is_current());
+  EXPECT_EQ(w.get_uid(), gl::context::get_current_window_uid());
+
+  gl::context::set_current(c2, w);
+  EXPECT_TRUE(c2.is_current());
+  EXPECT_EQ(w.get_uid(), gl::context::get_current_window_uid());
+}
+
+
+
+TEST_F(test_gl_context_optimizations, redundant_binding)
+{
+  static constexpr uint call_count = 1000u;
+
+  system_window w1("Test", vec2u(4u, 4u));
+  system_window w2("Test", vec2u(4u, 4u));
+  gl::context c1(gl::context_settings::get_default(), w1);
+  gl::context c2(gl::context_settings::get_default(), w1);
+
+  // First binding appears to be more expensive.
+  // Doing it here reduces bias in the measurements.
+  gl::context::set_current(c1, w1);
+  gl::context::set_current(c1, w2);
+  gl::context::set_current(c2, w1);
+  gl::context::set_current(c2, w2);
+
+  // Measurement clock.
+  hou::clock clk;
+
+  // Bind to same context window (should not rebind).
+  gl::context::unset_current();
+  clk = hou::clock();
+  for(uint i = 0; i < call_count; ++i)
+  {
+    gl::context::set_current(c1, w1);
+    gl::context::set_current(c1, w1);
+  }
+  std::chrono::nanoseconds time1 = clk.get_elapsed_time();
+
+  // Bind to different window (should rebind).
+  gl::context::unset_current();
+  clk = hou::clock();
+  for(uint i = 0; i < call_count; ++i)
+  {
+    gl::context::set_current(c1, w1);
+    gl::context::set_current(c1, w2);
+  }
+  std::chrono::nanoseconds time2 = clk.get_elapsed_time();
+
+  // Bind to different context (should rebind).
+  gl::context::unset_current();
+  clk = hou::clock();
+  for(uint i = 0; i < call_count; ++i)
+  {
+    gl::context::set_current(c1, w1);
+    gl::context::set_current(c2, w1);
+  }
+  std::chrono::nanoseconds time3 = clk.get_elapsed_time();
+
+  EXPECT_LT(time1, time2);
+  EXPECT_LT(time1, time3);
+}
