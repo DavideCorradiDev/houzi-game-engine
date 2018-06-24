e4e46d87b0a2168f5794985a10446d0774e11c50
Davide Corradi <davide.corradi.dev@gmail.com>
2018-06-24 10:50:04 +0200

    Print violated conditions in assertion erro messages.

--- source/houcor/include/hou/cor/assertions.hpp
+++ source/houcor/include/hou/cor/assertions.hpp
@@ -14,12 +14,12 @@
 namespace hou
 {
 
-/** Precondition violation error.
+/** Assertion violation error.
  *
- * This exception is thrown when a function precondition has been violated.
- * This reflects a programming error, and exection should be terminated.
+ * This exception is the base class for all exception classes used for
+ * assertions.
  */
-class HOU_COR_API precondition_violation : public exception
+class HOU_COR_API assertion_violation : public exception
 {
 public:
   /** Constructor.
@@ -28,9 +28,36 @@ public:
    *
    * \param line the line where the error happened.
    *
+   * \param message the error message condition.
+   *
+   * \param condition the violated condition.
+   *
    * \throws std::bad_alloc.
    */
-  precondition_violation(const std::string& path, uint line);
+  assertion_violation(const std::string& path, uint line,
+    const std::string& message, const std::string& condition);
+};
+
+/** Precondition violation error.
+ *
+ * This exception is thrown when a function precondition has been violated.
+ * This reflects a programming error, and exection should be terminated.
+ */
+class HOU_COR_API precondition_violation : public assertion_violation
+{
+public:
+  /** Constructor.
+   *
+   * \param path the path to the source file where the error happened.
+   *
+   * \param line the line where the error happened.
+   *
+   * \param condition the violated condition.
+   *
+   * \throws std::bad_alloc.
+   */
+  precondition_violation(
+    const std::string& path, uint line, const std::string& condition);
 };
 
 /** Postcondition violation error.
@@ -38,7 +65,7 @@ public:
  * This exception is thrown when a function postcondition has been violated.
  * This reflects a programming error, and exection should be terminated.
  */
-class HOU_COR_API postcondition_violation : public exception
+class HOU_COR_API postcondition_violation : public assertion_violation
 {
 public:
   /** Constructor.
@@ -47,9 +74,12 @@ public:
    *
    * \param line the line where the error happened.
    *
+   * \param condition the violated condition.
+   *
    * \throws std::bad_alloc.
    */
-  postcondition_violation(const std::string& path, uint line);
+  postcondition_violation(
+    const std::string& path, uint line, const std::string& condition);
 };
 
 /** Invariant violation error.
@@ -57,7 +87,7 @@ public:
  * This exception is thrown when an invariant has been violated.
  * This reflects a programming error, and exection should be terminated.
  */
-class HOU_COR_API invariant_violation : public exception
+class HOU_COR_API invariant_violation : public assertion_violation
 {
 public:
   /** Constructor.
@@ -66,20 +96,23 @@ public:
    *
    * \param line the line where the error happened.
    *
+   * \param condition the violated condition.
+   *
    * \throws std::bad_alloc.
    */
-  invariant_violation(const std::string& path, uint line);
+  invariant_violation(
+    const std::string& path, uint line, const std::string& condition);
 };
 
 }  // namespace hou
 
 #define HOU_PRECOND(condition)                                                 \
-  HOU_CHECK_0(condition, ::hou::precondition_violation)
+  HOU_CHECK_N(condition, ::hou::precondition_violation, #condition)
 
 #define HOU_POSTCOND(condition)                                                \
-  HOU_CHECK_0(condition, ::hou::postcondition_violation)
+  HOU_CHECK_N(condition, ::hou::postcondition_violation, #condition)
 
 #define HOU_INVARIANT(condition)                                               \
-  HOU_CHECK_0(condition, ::hou::invariant_violation)
+  HOU_CHECK_N(condition, ::hou::invariant_violation, #condition)
 
 #endif
--- source/houtest/include/hou/test.hpp
+++ source/houtest/include/hou/test.hpp
@@ -28,6 +28,20 @@ inline std::string get_exception_msg_regex(const hou::exception& ex)
         ' ', 1 + what_str.find_first_of('-', what_str.find_first_of(':')))));
 }
 
+inline std::string get_exception_msg_regex(const hou::assertion_violation& ex)
+{
+  // Assertions need a special treatment since they store the violated
+  // condition in the error message.
+  // Since this can't be easily known beforehand in tests, the condition will
+  // not be checked in the test, only if the rest of the message is correct.
+  std::string what_str = ex.what();
+  what_str = what_str.substr(what_str.find_first_not_of(
+    ' ', 1 + what_str.find_first_of('-', what_str.find_first_of(':'))));
+  what_str = what_str.substr(0u, what_str.find_first_of('('));
+
+  return std::string(".*:.* - ") + hou::escape_regex(what_str) + "\\(.*\\)\\.";
+}
+
 inline std::string get_exception_msg_regex(const std::exception& ex)
 {
   return hou::escape_regex(ex.what());
@@ -112,13 +126,13 @@ inline std::string get_exception_msg_regex(const std::exception& ex)
   EXPECT_ERROR_TEMPLATE(statement, exception_type, "", 0, __VA_ARGS__)
 
 #define EXPECT_PRECOND_ERROR(statement)                                        \
-  EXPECT_ERROR_0(statement, precondition_violation)
+  EXPECT_ERROR_N(statement, precondition_violation, "*")
 
 #define EXPECT_POSTCOND_ERROR(statement)                                       \
-  EXPECT_ERROR_0(statement, postcondition_violation)
+  EXPECT_ERROR_N(statement, postcondition_violation, "*")
 
 #define EXPECT_INVARIANT_ERROR(statement)                                      \
-  EXPECT_ERROR_0(statement, invariant_violation)
+  EXPECT_ERROR_N(statement, invariant_violation, "*")
 
 #if defined(HOU_DISABLE_EXCEPTIONS)
 #define EXPECT_NO_ERROR(statement)                                             \
