86395e73ebbdf0bbe44c01164fc89cc3c85a4533
Davide Corradi <davide.corradi.dev@gmail.com>
2018-07-01 08:49:37 +0200

    Fixed window icon color.

--- source/housys/src/hou/sys/window.cpp
+++ source/housys/src/hou/sys/window.cpp
@@ -244,7 +244,7 @@ void window::set_icon(const image2_rgba& icon)
     icon.get_size().y(),
     image2_rgba::pixel_bit_count,
     icon.get_size().x() * image2_rgba::pixel_byte_count,
-    0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
+    0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000);
   // clang-format on
   HOU_SDL_CHECK(surface != nullptr);
   SDL_SetWindowIcon(m_impl, surface);
