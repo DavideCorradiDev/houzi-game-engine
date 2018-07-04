880407cb02a747ab0b7d397dd25b97cf87216b17
Davide Corradi <davide.corradi.dev@gmail.com>
2018-07-04 23:38:17 +0200

    Compiled housys with Emscripten.

--- source/housys/include/hou/sys/display_format.hpp
+++ source/housys/include/hou/sys/display_format.hpp
@@ -19,11 +19,7 @@ namespace hou
 /**
  * Represents a display format.
  */
-#if defined HOU_EMSCRIPTEN
-enum class display_format : int32_t
-#else
 enum class display_format : uint32_t
-#endif
 {
   unknown = SDL_PIXELFORMAT_UNKNOWN,
   index1lsb = SDL_PIXELFORMAT_INDEX1LSB,
@@ -48,9 +44,9 @@ enum class display_format : uint32_t
   rgb24 = SDL_PIXELFORMAT_RGB24,
   bgr24 = SDL_PIXELFORMAT_BGR24,
   rgb888 = SDL_PIXELFORMAT_RGB888,
-  // rgbx8888 = SDL_PIXELFORMAT_RGBX8888,
+  rgbx8888 = SDL_PIXELFORMAT_RGBX8888,
   bgr888 = SDL_PIXELFORMAT_BGR888,
-  // bgrx8888 = SDL_PIXELFORMAT_BGRX8888,
+  bgrx8888 = SDL_PIXELFORMAT_BGRX8888,
   argb8888 = SDL_PIXELFORMAT_ARGB8888,
   rgba8888 = SDL_PIXELFORMAT_RGBA8888,
   abgr8888 = SDL_PIXELFORMAT_ABGR8888,
@@ -61,8 +57,8 @@ enum class display_format : uint32_t
   yuy2 = SDL_PIXELFORMAT_YUY2,
   uyvy = SDL_PIXELFORMAT_UYVY,
   yvyu = SDL_PIXELFORMAT_YVYU,
-  // nv12 = SDL_PIXELFORMAT_NV12,
-  // nv21 = SDL_PIXELFORMAT_NV21,
+  nv12 = SDL_PIXELFORMAT_NV12,
+  nv21 = SDL_PIXELFORMAT_NV21,
 };
 
 /**
