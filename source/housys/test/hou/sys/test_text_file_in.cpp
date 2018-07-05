f118de2049668f9fdf5a673d571723452a5eb79b
Davide Corradi <davide.corradi.dev@gmail.com>
2018-07-05 08:12:43 +0200

    Made input files work with Emscripten.

--- source/housys/test/hou/sys/test_text_file_in.cpp
+++ source/housys/test/hou/sys/test_text_file_in.cpp
@@ -26,7 +26,9 @@ public:
 
 public:
   static const std::string filename;
-  static const std::vector<uint8_t> file_content;
+  static const std::vector<char> file_content;
+  static const std::string gen_filename;
+  static const std::vector<char> gen_file_content;
 };
 
 using test_text_file_in_death_test = test_text_file_in;
@@ -36,30 +38,34 @@ using test_text_file_in_death_test = test_text_file_in;
 void test_text_file_in::SetUpTestCase()
 {
   Test::SetUpTestCase();
-  file f(filename, file_open_mode::write, file_type::binary);
-  f.write(file_content.data(), file_content.size());
+  file f(gen_filename, file_open_mode::write, file_type::binary);
+  f.write(gen_file_content.data(), gen_file_content.size());
 }
 
 
 
 void test_text_file_in::TearDownTestCase()
 {
-  remove_dir(filename);
+  remove_dir(gen_filename);
   Test::TearDownTestCase();
 }
 
 
 
 const std::string test_text_file_in::filename
+  = get_data_dir() + u8"test-file.txt";
+const std::vector<char> test_text_file_in::file_content
+  = {'a', 'b', 'c', 'd', 'e', 'f', '\n'};
+const std::string test_text_file_in::gen_filename
   = get_output_dir() + u8"test_text_file_in-\U00004f60\U0000597d.txt";
-const std::vector<uint8_t> test_text_file_in::file_content
+const std::vector<char> test_text_file_in::gen_file_content
   = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
 }  // namespace
 
 
 
-TEST_F(test_text_file_in, path_constructor)
+TEST_F(test_text_file_in, non_generated_file_path_constructor)
 {
   text_file_in fi(filename);
   EXPECT_FALSE(fi.eof());
@@ -68,9 +74,25 @@ TEST_F(test_text_file_in, path_constructor)
   EXPECT_EQ(0u, fi.get_read_byte_count());
   EXPECT_EQ(0u, fi.get_read_element_count());
   EXPECT_EQ(text_file_in::text_position::start, fi.get_text_pos());
-  std::vector<uint8_t> buffer(file_content.size(), 0u);
+  std::vector<char> buffer(file_content.size(), 0u);
   fi.read(buffer);
   EXPECT_EQ(buffer, file_content);
+}
+
+
+
+TEST_F(test_text_file_in, path_constructor)
+{
+  text_file_in fi(gen_filename);
+  EXPECT_FALSE(fi.eof());
+  EXPECT_FALSE(fi.error());
+  EXPECT_EQ(gen_file_content.size(), fi.get_byte_count());
+  EXPECT_EQ(0u, fi.get_read_byte_count());
+  EXPECT_EQ(0u, fi.get_read_element_count());
+  EXPECT_EQ(text_file_in::text_position::start, fi.get_text_pos());
+  std::vector<char> buffer(gen_file_content.size(), 0u);
+  fi.read(buffer);
+  EXPECT_EQ(buffer, gen_file_content);
 }
 
 
@@ -86,24 +108,24 @@ TEST_F(test_text_file_in_death_test, path_constructor_failure)
 
 TEST_F(test_text_file_in, move_constructor)
 {
-  text_file_in fi_dummy(filename);
+  text_file_in fi_dummy(gen_filename);
   text_file_in fi(std::move(fi_dummy));
   EXPECT_FALSE(fi.eof());
   EXPECT_FALSE(fi.error());
-  EXPECT_EQ(file_content.size(), fi.get_byte_count());
+  EXPECT_EQ(gen_file_content.size(), fi.get_byte_count());
   EXPECT_EQ(0u, fi.get_read_byte_count());
   EXPECT_EQ(0u, fi.get_read_element_count());
   EXPECT_EQ(text_file_in::text_position::start, fi.get_text_pos());
-  std::vector<uint8_t> buffer(file_content.size(), 0u);
+  std::vector<char> buffer(gen_file_content.size(), 0u);
   fi.read(buffer);
-  EXPECT_EQ(buffer, file_content);
+  EXPECT_EQ(buffer, gen_file_content);
 }
 
 
 
 TEST_F(test_text_file_in, set_text_pos)
 {
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   std::string buffer(2, 0);
   EXPECT_EQ(text_file_in::text_position::start, fi.get_text_pos());
   fi.read(buffer);
@@ -122,21 +144,21 @@ TEST_F(test_text_file_in, read_to_variable)
   using buffer_type = uint16_t;
   static constexpr size_t buffer_byte_size = sizeof(buffer_type);
 
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   buffer_type buffer;
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(1u, fi.get_read_element_count());
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(&buffer), file_content.data(), buffer_byte_size);
+    reinterpret_cast<char*>(&buffer), gen_file_content.data(), buffer_byte_size);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(1u, fi.get_read_element_count());
-  const uint8_t* offset_data = file_content.data() + buffer_byte_size;
+  const char* offset_data = gen_file_content.data() + buffer_byte_size;
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(&buffer), offset_data, buffer_byte_size);
+    reinterpret_cast<char*>(&buffer), offset_data, buffer_byte_size);
 }
 
 
@@ -147,21 +169,21 @@ TEST_F(test_text_file_in, read_to_basic_array)
   static constexpr size_t buffer_size = 3u;
   static constexpr size_t buffer_byte_size = sizeof(buffer_type) * buffer_size;
 
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   buffer_type buffer[buffer_size];
 
   fi.read(buffer, buffer_size);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(buffer), file_content.data(), buffer_byte_size);
+    reinterpret_cast<char*>(buffer), gen_file_content.data(), buffer_byte_size);
 
   fi.read(buffer, buffer_size);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  const uint8_t* offset_data = file_content.data() + buffer_byte_size;
+  const char* offset_data = gen_file_content.data() + buffer_byte_size;
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(buffer), offset_data, buffer_byte_size);
+    reinterpret_cast<char*>(buffer), offset_data, buffer_byte_size);
 }
 
 
@@ -172,21 +194,21 @@ TEST_F(test_text_file_in, read_to_array)
   static constexpr size_t buffer_size = 3u;
   static constexpr size_t buffer_byte_size = sizeof(buffer_type) * buffer_size;
 
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   std::array<buffer_type, buffer_size> buffer = {0, 0, 0};
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  EXPECT_ARRAY_EQ(reinterpret_cast<uint8_t*>(buffer.data()),
-    file_content.data(), buffer_byte_size);
+  EXPECT_ARRAY_EQ(reinterpret_cast<char*>(buffer.data()),
+    gen_file_content.data(), buffer_byte_size);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  const uint8_t* offset_data = file_content.data() + buffer_byte_size;
+  const char* offset_data = gen_file_content.data() + buffer_byte_size;
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(buffer.data()), offset_data, buffer_byte_size);
+    reinterpret_cast<char*>(buffer.data()), offset_data, buffer_byte_size);
 }
 
 
@@ -197,21 +219,21 @@ TEST_F(test_text_file_in, read_to_vector)
   static constexpr size_t buffer_size = 3u;
   static constexpr size_t buffer_byte_size = sizeof(buffer_type) * buffer_size;
 
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   std::vector<buffer_type> buffer(buffer_size, 0u);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  EXPECT_ARRAY_EQ(reinterpret_cast<uint8_t*>(buffer.data()),
-    file_content.data(), buffer_byte_size);
+  EXPECT_ARRAY_EQ(reinterpret_cast<char*>(buffer.data()),
+    gen_file_content.data(), buffer_byte_size);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  const uint8_t* offset_data = file_content.data() + buffer_byte_size;
+  const char* offset_data = gen_file_content.data() + buffer_byte_size;
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(buffer.data()), offset_data, buffer_byte_size);
+    reinterpret_cast<char*>(buffer.data()), offset_data, buffer_byte_size);
 }
 
 
@@ -222,20 +244,20 @@ TEST_F(test_text_file_in, read_to_string)
   static constexpr size_t buffer_size = 3u;
   static constexpr size_t buffer_byte_size = sizeof(buffer_type) * buffer_size;
 
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   std::string buffer(buffer_size, 0);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  EXPECT_ARRAY_EQ(reinterpret_cast<const uint8_t*>(buffer.data()),
-    file_content.data(), buffer_byte_size);
+  EXPECT_ARRAY_EQ(reinterpret_cast<const char*>(buffer.data()),
+    gen_file_content.data(), buffer_byte_size);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  const uint8_t* offset_data = file_content.data() + buffer_byte_size;
-  EXPECT_ARRAY_EQ(reinterpret_cast<const uint8_t*>(buffer.data()),
+  const char* offset_data = gen_file_content.data() + buffer_byte_size;
+  EXPECT_ARRAY_EQ(reinterpret_cast<const char*>(buffer.data()),
     offset_data, buffer_byte_size);
 }
 
@@ -247,61 +269,61 @@ TEST_F(test_text_file_in, read_to_span)
   static constexpr size_t buffer_size = 3u;
   static constexpr size_t buffer_byte_size = sizeof(buffer_type) * buffer_size;
 
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   std::vector<buffer_type> vec(buffer_size, 0u);
   span<buffer_type> buffer(vec);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  EXPECT_ARRAY_EQ(reinterpret_cast<uint8_t*>(buffer.data()),
-    file_content.data(), buffer_byte_size);
+  EXPECT_ARRAY_EQ(reinterpret_cast<char*>(buffer.data()),
+    gen_file_content.data(), buffer_byte_size);
 
   fi.read(buffer);
   EXPECT_EQ(buffer_byte_size, fi.get_read_byte_count());
   EXPECT_EQ(buffer_size, fi.get_read_element_count());
-  const uint8_t* offset_data = file_content.data() + buffer_byte_size;
+  const char* offset_data = gen_file_content.data() + buffer_byte_size;
   EXPECT_ARRAY_EQ(
-    reinterpret_cast<uint8_t*>(buffer.data()), offset_data, buffer_byte_size);
+    reinterpret_cast<char*>(buffer.data()), offset_data, buffer_byte_size);
 }
 
 
 
 TEST_F(test_text_file_in, read_all_to_vector)
 {
-  text_file_in fi(filename);
-  auto fi_content = fi.read_all<std::vector<uint8_t>>();
+  text_file_in fi(gen_filename);
+  auto fi_content = fi.read_all<std::vector<char>>();
 
-  EXPECT_EQ(file_content, fi_content);
-  EXPECT_EQ(file_content.size(), fi.get_read_byte_count());
+  EXPECT_EQ(gen_file_content, fi_content);
+  EXPECT_EQ(gen_file_content.size(), fi.get_read_byte_count());
 }
 
 
 
 TEST_F(test_text_file_in, read_all_to_vector_not_from_start)
 {
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
 
   std::string buffer(2, 0);
   EXPECT_EQ(text_file_in::text_position::start, fi.get_text_pos());
   fi.read(buffer);
   EXPECT_NE(text_file_in::text_position::start, fi.get_text_pos());
 
-  auto fi_content = fi.read_all<std::vector<uint8_t>>();
+  auto fi_content = fi.read_all<std::vector<char>>();
 
-  EXPECT_EQ(file_content, fi_content);
-  EXPECT_EQ(file_content.size(), fi.get_read_byte_count());
+  EXPECT_EQ(gen_file_content, fi_content);
+  EXPECT_EQ(gen_file_content.size(), fi.get_read_byte_count());
 }
 
 
 
 TEST_F(test_text_file_in, eof)
 {
-  text_file_in fi(filename);
+  text_file_in fi(gen_filename);
   uint count = 0;
   while(!fi.eof())
   {
-    uint8_t buffer;
+    char buffer;
     fi.read(buffer);
     ++count;
   }
